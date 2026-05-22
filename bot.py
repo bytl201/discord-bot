f1f0d8cb63cd68feb47d544beae8a19b89aedb87
tlobden23 <tlobden79@gmail.com>
2026-05-22 11:23:32 -0400

    Add hello command and fix bot permissions

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hey {ctx.author.name}!")
+
+bot.run(TOKEN)
